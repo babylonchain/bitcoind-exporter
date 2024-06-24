2bd096b039cadec00c69e1d550b995132aaa0c53
Hiep Mai <hiep.mai@babylonchain.io>
2024-06-24 11:08:52 +0700

    add walletMonitor

--- metricsHandler.js
+++ metricsHandler.js
@@ -22,6 +22,7 @@ const {
     rpchost = '127.0.0.1',
     rpcport = '8332',
     rpcscheme = 'http',
+    walletMonitor = true
 } = process.env;
 
 const call = createCall({
@@ -35,61 +36,56 @@ const call = createCall({
 const metricsHandler = (req, res) => {
     res.set('Content-Type', register.contentType);
 
-    const listUnspentPromise = call('listunspent')
-        .then(transactions => transactions.reduce((balances, transaction) => {
-            balances[transaction.address] = (balances[transaction.address] || 0)  + transaction.amount;
+    const promises = []
+    if (walletMonitor) {
+        const listUnspentPromise = call('listunspent')
+            .then(transactions => transactions.reduce((balances, transaction) => {
+                balances[transaction.address] = (balances[transaction.address] || 0) + transaction.amount;
 
-            return balances;
-        }, {}))
-        .then(balances => Object
-            .keys(balances)
-            .forEach((address) => addressBalanceMetric.set({ address }, balances[address]))
-        )
-    ;
-    const walletInfoPromise = call('getwalletinfo')
-        .then(
-            ({
-                 unconfirmed_balance,
-                 immature_balance,
-                 balance,
-                 walletversion,
-                 txcount,
-                 keypoololdest,
-                 keypoolsize,
-                 unlocked_until,
-                 paytxfee,
-             }) => {
+                return balances;
+            }, {}))
+            .then(balances => Object
+                .keys(balances)
+                .forEach((address) => addressBalanceMetric.set({ address }, balances[address]))
+            );
+        const walletInfoPromise = call('getwalletinfo')
+            .then(
+                ({
+                    unconfirmed_balance,
+                    immature_balance,
+                    balance,
+                    walletversion,
+                    txcount,
+                    keypoolsize,
+                    unlocked_until,
+                    paytxfee,
+                }) => {
 
 
-                walletVersionMetric.set({ ticker }, walletversion);
-                walletBalanceMetric.set({ status: 'unconfirmed' }, unconfirmed_balance);
-                walletBalanceMetric.set({ status: 'immature' }, immature_balance);
-                walletBalanceMetric.set({ status: 'confirmed' }, balance);
-                walletTransationsMetric.set(txcount);
-                unlockedUntilMetric.set(unlocked_until || 0);
-                keyPoolOldestMetric.set(keypoololdest);
-                keyPoolSizeMetric.set(keypoolsize);
-                transactionFeeMetric.set(paytxfee);
-            }
-        )
-    ;
+                    walletVersionMetric.set({ ticker }, walletversion);
+                    walletBalanceMetric.set({ status: 'unconfirmed' }, unconfirmed_balance);
+                    walletBalanceMetric.set({ status: 'immature' }, immature_balance);
+                    walletBalanceMetric.set({ status: 'confirmed' }, balance);
+                    walletTransationsMetric.set(txcount);
+                    unlockedUntilMetric.set(unlocked_until || 0);
+                    keyPoolSizeMetric.set(keypoolsize);
+                    transactionFeeMetric.set(paytxfee);
+                }
+            );
+        promises.push(listUnspentPromise, walletInfoPromise);
+    }
+
     const bestBlockPromise = call('getbestblockhash')
         .then(hash => call('getblock', hash))
         .then(bestBlockInfo => {
             bestBlockIndexMetric.set(bestBlockInfo.height);
             bestBlockTimeMetric.set(bestBlockInfo.time);
-        })
-    ;
+        });
     const difficultyPromise = call('getdifficulty')
-        .then(difficulty => difficultyMetric.set(difficulty))
-    ;
+        .then(difficulty => difficultyMetric.set(difficulty));
 
-    Promise.all([
-        listUnspentPromise,
-        walletInfoPromise,
-        bestBlockPromise,
-        difficultyPromise
-    ])
+    promises.push(bestBlockPromise, difficultyPromise);
+    Promise.all(promises)
         .then(() => res.end(register.metrics()))
         .catch((error) => {
             console.error(error);
@@ -102,8 +98,7 @@ const metricsHandler = (req, res) => {
             }
 
             return res.status(code).send(`# ${error.message}\n`)
-        })
-    ;
+        });
 };
 
 module.exports = metricsHandler;
